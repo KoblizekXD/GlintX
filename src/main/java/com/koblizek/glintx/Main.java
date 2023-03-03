02f76ccb24d101d30b4eb43a5aacc9197385e72f
Slosa <torvalds@osdl.org>
2023-03-03 17:49:07 +0100

    Intial commit (2)

---
+++ src/main/java/com/koblizek/glintx/Main.java
@@ -0,0 +1,10 @@
+package com.koblizek.glintx;
+
+import com.koblizek.glintx.main.GLMain;
+
+public class Main {
+    public static void main(String[] args) {
+        // starts the application
+        new GLMain().run();
+    }
+}
