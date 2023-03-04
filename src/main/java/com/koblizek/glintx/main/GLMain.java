5c4ac8506f999b8db44bcf8afb69ea4b802279ee
KoblizekXD <janprokupek@centrum.cz>
2023-03-04 18:53:46 +0100

    cleanup

--- src/main/java/com/koblizek/glintx/main/GLMain.java
+++ src/main/java/com/koblizek/glintx/main/GLMain.java
@@ -1,6 +1,7 @@
 package com.koblizek.glintx.main;
 
 import com.koblizek.glintx.api.Window;
+import com.koblizek.glintx.api.WindowPosition;
 import com.koblizek.glintx.imgui.GuiWrapper;
 import org.apache.logging.log4j.LogManager;
 import com.koblizek.glintx.api.resource.image.GLImage;
@@ -59,50 +60,21 @@ public class GLMain {
         if ( !glfwInit() )
             LOGGER.fatal("Unable to initialize GLFW");
 
-        // Configure GLFW
-        glfwDefaultWindowHints(); // optional, the current window hints are already the default
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
-
-        // Create the window
-        window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
+        Window window1 = Window.createNewWindow(300, 300, "ano");
+        this.window = window1.getHandle();
         if ( window == NULL )
             LOGGER.fatal("Failed to create the GLFW window");
-        Window window1 = new Window(window);
-
-        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
-        /*glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
-                glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
-        });*/
         window1.addKeyPressEvent(key -> {
             LOGGER.info("Key: {} has been pressed!", key);
         });
         window1.setKeyCallbacks();
 
         // Get the thread stack and push a new frame
-        try ( MemoryStack stack = stackPush() ) {
-            IntBuffer pWidth = stack.mallocInt(1); // int*
-            IntBuffer pHeight = stack.mallocInt(1); // int*
+        window1.setPosition(WindowPosition.CENTER);
 
-            // Get the window size passed to glfwCreateWindow
-            glfwGetWindowSize(window, pWidth, pHeight);
-
-            // Get the resolution of the primary monitor
-            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-            // Center the window
-            glfwSetWindowPos(
-                    window,
-                    (vidmode.width() - pWidth.get(0)) / 2,
-                    (vidmode.height() - pHeight.get(0)) / 2
-            );
-        } // the stack frame is popped automatically
-
-        // Make the OpenGL context current
-        glfwMakeContextCurrent(window);
+        window1.glfw_context_set();
         // Enable v-sync
-        glfwSwapInterval(1);
+        window1.toggleVSync(true);
 
         //set icon image
         GLFWImage image = GLFWImage.malloc(); GLFWImage.Buffer imagebf = GLFWImage.malloc(1);
@@ -115,19 +87,9 @@ public class GLMain {
     }
 
     private void loop() {
-        // This line is critical for LWJGL's interoperation with GLFW's
-        // OpenGL context, or any context that is managed externally.
-        // LWJGL detects the context that is current in the current thread,
-        // creates the GLCapabilities instance and makes the OpenGL
-        // bindings available for use.
         GL.createCapabilities();
-        //findme :: starts rendering gui
         gui.start(window);
-        // Set the clear color
         glClearColor(.5f, .5f, .5f, 0.0f);
-
-        // Run the rendering loop until the user has attempted to close
-        // the window or has pressed the ESCAPE key.
         while ( !glfwWindowShouldClose(window) ) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
 
@@ -136,12 +98,7 @@ public class GLMain {
 
             glfwSwapBuffers(window); // swap the color buffers
 
-            // Poll for window events. The key callback above will only be
-            // invoked during this call.
             glfwPollEvents();
         }
     }
-
-
-
 }
